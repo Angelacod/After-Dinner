65361a8f30da9c144eac74bf1b63ea13faa04033
Angelacod <47693807+Angelacod@users.noreply.github.com>
2019-04-21 21:54:04 +0100

    Test: to see if i can make chnages to the contract form so that it works correctly

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>After Dinner: About</title>
+    <link rel="icon" href="images\logo.png" type="image/png" sizes="16x16">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/.css">
+  </head>
+
+<body>
+
+
+
+  </body>
+</html>
